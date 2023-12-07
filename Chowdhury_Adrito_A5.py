8c32951cef539f4c359d99af9b9e81d551d77192
Adrito <Ayaan.Adrito@student.tdsb.on.ca>
2023-12-07 14:56:56 -0500

    Add filler text generator

--- Chowdhury_Adrito_A5.py
+++ Chowdhury_Adrito_A5.py
@@ -58,6 +58,7 @@ def clean_text(text: str) -> str:
     new_text = ""
 
     for char in text:
+        # 65 and 90 are the ascii values for between A and Z
         if 65 <= ord(char) <= 90:
             new_text += char
         else:
@@ -65,6 +66,27 @@ def clean_text(text: str) -> str:
 
     return(new_text)
 
+def random_char(num1: int, num2: int) -> str:
+    """Generate a random character betweet ASCII values num1 and num2
+    and return it."""
+
+    return chr(random.randint(num1, num2))
+
+def add_filler(text: str, num: int) -> str:
+    """Add random letters to text until its length
+    is divisible by num.
+
+    >>> filler_text('HELL', 5)
+    'HELLT'
+    >>> filler_text('RECTANGLE', 4)
+    'RECTANGLEQSF'
+    """
+
+    result = text
+    while len(result) % num != 0:
+        result += random_char(65, 90)
+
+    return result
 
 def add_spaces(text: str, spacer: int) -> str:
     """Add a space to text every spacer characters.
@@ -95,16 +117,19 @@ def encrypt_text(plain_text: str, key: str) -> str:
     key_counter = 0
 
     for i in range(len(plain_text)):
-        if key_counter <= len(key):
+        if key_counter >= len(key):
             key_counter = 0
 
+        # Calculate how many spaces to shift by
         shift = ALPHABET.index(key[key_counter]) + 1
+        # Shift the current character to make the encrypted charcter
         new_char = (ALPHABET.index(plain_text[i]) + shift) % 25
         result += ALPHABET[new_char]
 
         key_counter += 1
 
-    return result
+    # Add filler text to result before returning it spaced out
+    return add_spaces(add_filler(result, 5), 5)
 
 def decrypt_text(cipher_text: str, key: str) -> str:
     """Decrypt_text message cipher_text using decoder key.
@@ -120,38 +145,42 @@ def decrypt_text(cipher_text: str, key: str) -> str:
     key_counter = 0
 
     for i in range(len(cipher_text)):
-        if key_counter <= len(key):
+        if key_counter >= len(key):
             key_counter = 0
 
+        # Calculate how many spaces to shift by
         shift = ALPHABET.index(key[key_counter]) + 1
+        # Shift the current character to make the decrypted charcter
         new_char = (ALPHABET.index(cipher_text[i]) - shift)
 
+        # Shift character sharting from Z if less than A
         if new_char < 0:
-            new_char += 25
+            new_char += 26
 
         result += ALPHABET[new_char]
 
         key_counter += 1
 
-    return result
+    # Add spaces every 5 characters before returning
+    return add_spaces(result, 5)
 
 def main():
     SPACER = 5
     running = True
 
-    while running == True:
-        final_message = ""
-        print("""----------------------------------
+    print("""----------------------------------
 EasyCrypt Text Encryptor/Decryptor
 ----------------------------------""")
+
+    while running == True:
+        final_message = ""
         primary_input = validate_input(1, 4, "invalid input", "Invalid choice. Try again.")
         if primary_input == 1:
-
-            plain_text = input("Please enter text to encrypt: ")
+            plain_text = input("\nPlease enter text to encrypt: ")
             plain_text = clean_text(plain_text)
 
             print_text = add_spaces(plain_text, SPACER)
-            print("This is the plaintext: {}".format(print_text))
+            print(f"This is the plaintext: {print_text}\n")
 
             while True:
                 key = input("A key is any string of letters (1-500 chars): ")
@@ -161,16 +190,16 @@ EasyCrypt Text Encryptor/Decryptor
                     break
                 else:
                     print("invalid length")
-            print("Using encryption key: {}".format(key))
+            print(f"Using encryption key: {key}\n")
 
             final_message = encrypt_text(plain_text, key)
-            print(final_message)
+            print(f"Your message has been encrypted:\n{final_message}\n")
         if primary_input == 2:
-            cipher_text = input("Please enter text to decrypt: ")
-            cipher_text = clean_text(plain_text)
+            cipher_text = input("\nPlease enter text to decrypt: ")
+            cipher_text = clean_text(cipher_text)
 
-            cipher_text = add_spaces(plain_text, SPACER)
-            print("This is the plaintext: {}".format(print_text))
+            print_text = add_spaces(cipher_text, SPACER)
+            print(f"This is the plaintext: {print_text}\n")
 
             while True:
                 key = input("A key is any string of letters (1-500 chars): ")
@@ -180,10 +209,10 @@ EasyCrypt Text Encryptor/Decryptor
                     break
                 else:
                     print("invalid length")
-            print("Using encryption key: {}".format(key))
+            print(f"Using encryption key: {key}\n")
 
             final_message = decrypt_text(cipher_text, key)
-            print(final_message)
+            print(f"Your message has been decrypted:\n{final_message}\n")
         if primary_input == 3:
             print("Generate key")
         if primary_input == 4:
