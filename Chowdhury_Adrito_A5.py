4c8424aa409d2900852ec0e29c580571fd8bb160
Ayaan Adrito <ayaan.adrito@gmail.com>
2023-12-07 23:05:01 -0500

    Add generate key function

--- Chowdhury_Adrito_A5.py
+++ Chowdhury_Adrito_A5.py
@@ -21,26 +21,24 @@ def print_menu():
 3. Generate key.
 4. Exit.""")
 
-def validate_input(num1: int, num2: int, msg1: str, msg2: str) -> int:
+def validate_input(num1: int, num2: int, msg: str, err_msg1: str, err_msg2: str) -> int:
     """Validate input and return an integer between num1 and num2.
-    Print erorr message 1: msg1 if input is not and int.
-    Print erorr message 2: msg2 if int is out of range."""
+    Print erorr message 1: err_msg1 if input is not and int.
+    Print erorr message 2: err_msg2 if int is out of range."""
 
     valid = False
-
-    print_menu()
 
     while valid == False:
         while True:    
             try:
-                primary_input = int(input("> "))
+                primary_input = int(input(msg))
                 break
             except ValueError:
-                print(msg1)
+                print(err_msg1)
         if num1 <= primary_input <= num2:
             valid = True
         else:
-            print(msg2)
+            print(err_msg2)
             valid = False
     return(primary_input) 
 
@@ -48,10 +46,10 @@ def clean_text(text: str) -> str:
     """Return text with letters only and uppercased.
     
     >>> clean_text('ABCJSHD@#(I#2$SDA)')
-    ABCJSHDSDA
+    'ABCJSHDSDA'
 
     >>> clean_text('cH1+ick#en')
-    CHICKEN
+    'CHICKEN'
     """
 
     text = text.upper()
@@ -84,6 +82,15 @@ def add_filler(text: str, num: int) -> str:
 
     result = text
     while len(result) % num != 0:
+        result += random_char(65, 90)
+
+    return result
+
+def generate_key(num: int) -> str:
+    """Generate and return a key with a length of num"""
+
+    result = ""
+    while len(result) <= num:
         result += random_char(65, 90)
 
     return result
@@ -173,8 +180,11 @@ EasyCrypt Text Encryptor/Decryptor
 ----------------------------------""")
 
     while running == True:
+
         final_message = ""
-        primary_input = validate_input(1, 4, "invalid input", "Invalid choice. Try again.")
+        print_menu()
+        primary_input = validate_input(1, 4, "> ", "invalid input", "Invalid choice. Try again.")
+
         if primary_input == 1:
             plain_text = input("\nPlease enter text to encrypt: ")
             plain_text = clean_text(plain_text)
@@ -214,7 +224,13 @@ EasyCrypt Text Encryptor/Decryptor
             final_message = decrypt_text(cipher_text, key)
             print(f"Your message has been decrypted:\n{final_message}\n")
         if primary_input == 3:
-            print("Generate key")
+            print("\nGenerate an encryption key comprised of random characters (max 500).")
+
+            msg = "Enter the desired length of key: "
+            desired_length = validate_input(1, 500, msg, "invalid input", "invalid length")
+            key = generate_key(desired_length)
+            print(f"Your new encryption key: \n{key}\n")
+
         if primary_input == 4:
             running = False
             break
